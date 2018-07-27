8d34de7f772dea3580cf6117956cb353cc14d748
John Colagioia <john@colagioia.net>
2018-07-27 06:52:45 -0400

    Extract commits on all branches

--- graft.js
+++ graft.js
@@ -1,7 +1,39 @@
 var Git = require("nodegit");
 
+let branches = {};
+
 Git.Repository.open(".")
   .then(function(repo) {
+    return { names: repo.getReferenceNames(Git.Reference.TYPE.LISTALL), repo };
+  }).then(function(r) {
+    let { names, repo } = r;
+    names.then(function(nn) {
+      for (let ni = 0; ni < nn.length; ni++) {
+        let name = nn[ni];
+        branches[name] = [];
+        try {
+        repo.getBranchCommit(name).then(function(firstCommit) {
+          let history = firstCommit.history();
+          let count = 0;
+          history.on("commit", function(commit) {
+            branches[name].push(commit);
+            /*
+            if (++count >= 5) {
+              return;
+            }
+            console.log(`[[Branch: ${name}]]`)
+            console.log("commit " + commit.sha());
+            var author = commit.author();
+            console.log("Author:\t" + author.name() + " <" + author.email() + ">");
+            console.log("Date:\t" + commit.date());
+            console.log("\n    " + commit.message());
+            */
+          });
+          history.start();
+        });
+        } catch(e) {
+        console.log(e);
+        }
       }
     });
   });
