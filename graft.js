a89f9391ceeb393e3b83f55c495f7603a19efb08
John Colagioia <john@colagioia.net>
2018-07-28 08:51:51 -0400

    Collect commits centrally for later processing (output, in this case)

--- graft.js
+++ graft.js
@@ -1,6 +1,8 @@
 var Git = require("nodegit");
 
 let branches = {};
+let semaphore = 0;
+let intervalId = 0;
 
 Git.Repository.open(".")
   .then(function(repo) {
@@ -8,6 +10,8 @@ Git.Repository.open(".")
   }).then(function(r) {
     let { names, repo } = r;
     names.then(function(nn) {
+      semaphore = nn.length;
+      intervalId = setInterval(reportBranches, 100);
       for (let ni = 0; ni < nn.length; ni++) {
         let name = nn[ni];
         branches[name] = [];
@@ -16,20 +20,11 @@ Git.Repository.open(".")
           let history = firstCommit.history();
           let count = 0;
           history.on("commit", function(commit) {
-            branches[name].push(commit);
-            /*
-            if (++count >= 5) {
-              return;
-            }
-            console.log(`[[Branch: ${name}]]`)
-            console.log("commit " + commit.sha());
-            var author = commit.author();
-            console.log("Author:\t" + author.name() + " <" + author.email() + ">");
-            console.log("Date:\t" + commit.date());
-            console.log("\n    " + commit.message());
-            */
+            branches[name].unshift(commit);
           });
           history.start();
+        }).then(function() {
+          --semaphore;
         });
         } catch(e) {
         console.log(e);
@@ -38,3 +33,23 @@ Git.Repository.open(".")
     });
   });
 
+function reportBranches() {
+  if (semaphore > 0) {
+    return;
+  }
+  clearInterval(intervalId);
+  let keys = Object.keys(branches);
+  for (let k = 0; k < keys.length; k++) {
+    let branch = keys[k];
+    let commits = branches[branch];
+    console.log(`\n[[Branch:  ${branch}]]`);
+    for (let c = 0; c < commits.length; c++) {
+      let commit = commits[c];
+      console.log(`\ncommit\t${commit.sha()}`);
+      var author = commit.author();
+      console.log(`Author:\t${author.name()} <${author.email()}>`);
+      console.log(`Date:\t${commit.date()}`);
+      console.log(`\n\t${commit.message().trim()}`);
+    }
+  }
+}
